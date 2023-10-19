32da7c785272708da610fdd30266362ed162004c
Salah eddine berret <146879159+SBerret3301@users.noreply.github.com>
2023-10-19 02:29:26 +0100

    Add files via upload

---
+++ password.py
@@ -0,0 +1,11 @@
+for i in range(3) :  
+    n = input("enter the password : ")
+    if n == "DEV@2023" :
+        print("Hello")
+        break
+if n != "DEV@2023" :
+    a = input("enter your favorit writer : ")
+    if a == "eiichero oda" :
+        print("Hello")
+    else :
+       print("False")
